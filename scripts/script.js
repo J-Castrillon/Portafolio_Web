58602b8bd2c5df7fece716dee21c433903bbfc1c
Julián Castrillón <juliancastrillon681@gmail.com>
2022-11-26 19:53:26 -0500

    Cambios para las redes sociales, se le colocó mediante JavaScript los href correspondientes

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,15 @@
 'use strict'; 
 
 window.addEventListener('load',()=>{
+    // Declaración e inicialización de variables ; 
+    const nets_instagram = document.querySelector('#nets-instragram'); 
+    const nets_linkedin = document.querySelector('#nets-linkedin'); 
+    const nets_github = document.querySelector('#nets-github'); 
     
+
+    // Instrucciones; 
+    // Redes sociales - versión mobile; 
+    nets_instagram.setAttribute('href','https://www.instagram.com/juliancastrillon17/');
+    nets_github.setAttribute('href','https://github.com/J-Castrillon?tab=repositories'); 
+    nets_linkedin.setAttribute('href','https://www.linkedin.com/in/juli%C3%A1n-castrill%C3%B3n-364056168/'); 
 }); 
